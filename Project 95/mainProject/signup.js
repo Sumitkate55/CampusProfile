0eb6a82e8a13043621a19baad9e604cc96ddf8d7
Sumit Kate <sumit.kate23@pcu.edu.in>
2024-08-11 18:41:09 +0530

    added index.html

---
+++ Project 95/mainProject/signup.js
@@ -0,0 +1,7 @@
+document.getElementById('open-login-btn').addEventListener('click', function() {
+    window.location.href = 'login.html';
+});
+
+document.getElementById('login-link').addEventListener('click', function() {
+    window.location.href = 'login.html';
+});
